6ff3fdb1efc196d6ff7824e1352e0a22db244134
wardddddd <my48796@gmail.com>
2017-03-13 07:16:13 -0500

    106/03/13LAB2
    106/03/13LAB2

---
+++ lab2.ino
@@ -0,0 +1,15 @@
+float Analogin = 0;
+
+void setup() {
+ pinMode(A0,INPUT);
+Serial.begin(9600);
+}
+void loop() {
+ int i;
+ float Vin;
+ for (i=0;i<=4;i++)
+Analogin = 0.7* Analogin + 0.3* analogRead(A0);
+Vin =  Analogin* 5 / 1024;
+Serial.println(Vin);
+delay(1000);
+}
