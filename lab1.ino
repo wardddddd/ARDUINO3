d4e91449c4cc838203bef856a00960c1d7f35293
wardddddd <my48796@gmail.com>
2017-03-13 07:15:29 -0500

    106/03/13LAB1
    106/03/13LAB1

---
+++ lab1.ino
@@ -0,0 +1,29 @@
+const byte LED = 13;
+void setup() {
+  pinMode(LED, OUTPUT);
+
+ Serial.begin(9600);
+ Serial.println("Hello,World");
+
+}
+
+void loop() 
+{
+  if(Serial.available())
+  {
+    char val;
+    val = Serial.read();
+   
+    switch(val)
+    {
+    case  '0' :
+      digitalWrite(LED,LOW);
+      Serial.println("LED OFF");
+      break;
+    case  '1' :
+      digitalWrite(LED,HIGH);
+      Serial.println("LED ON");
+      break;
+    }
+  }
+}
