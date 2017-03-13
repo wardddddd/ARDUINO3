75b907f0b5bbe52115a35e70fa8e927a268a6498
wardddddd <my48796@gmail.com>
2017-03-13 07:19:30 -0500

    106/03/13LAB4
    106/03/13LAB4

---
+++ lab4.ino
@@ -0,0 +1,15 @@
+double sensorValue;
+double sensorVoltage ;
+void setup() {
+ pinMode(A1,INPUT);
+ Serial.begin(9600);
+}
+void loop() {
+  double Vin=5;
+  int i;
+ for(i=0;i<=4;i++)
+sensorValue = 0.7* sensorValue + 0.3* analogRead(A1);
+sensorVoltage = sensorValue * 5 / 1024;
+Serial.println(sensorVoltage );
+delay(500);
+}
